92a26881745806fa939c7f48e20bfe14454a7afe
KangWoosung <wkang386@gmail.com>
2023-09-20 12:03:33 +0900

    배포판의 두번째 업데이트.. 자동적용 모니터링 2

--- src/client/Home.jsx
+++ src/client/Home.jsx
@@ -15,6 +15,13 @@ export default function Home() {
           <li>내일은 감자볶음</li>
           <li>모래는 감자찜</li>
         </ul>
+        <div>
+          업데이트 작업 후에, gitHub 에 main 브랜치에 push 하면,
+          <br />
+          Vercel 에서 자동으로 업데이트를 적용해준다.
+          <br />
+          와... 신세계...ㄷㄷㄷ
+        </div>
       </div>
     </>
   );
